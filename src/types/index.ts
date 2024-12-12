bd9955be306e5738635ad2ed953cc0e099d69823
Laktima <ms.pankina.97@yandex.ru>
2024-12-12 22:11:33 +0300

    Описана часть документации. Определены основные интерфейсы

---
+++ src/types/index.ts
@@ -0,0 +1,38 @@
+interface IProduct {
+    id: string;
+    title: string;
+    category: string;
+    description: string;
+    price: number | null; 
+    image: string;
+}
+
+interface IPayment {
+    adress: string;
+    telNum: string;
+    email: string;
+    paymentType: string;
+    buy(): void;
+}
+
+interface IBasketModel {
+    items: Map<string, number>;
+    add(id:string): void;
+    remove(id: string): void;
+    getTotalPrice(): number;
+    goToPayment(): void;
+}
+
+interface CatalogModel {
+    items: IProduct[];
+    setItems(items: IProduct[]): void;
+    getProduct(id: string): IProduct;
+}
+
+interface IView {
+    render(data?: object): HTMLElement;
+}
+
+interface IViewConstructor {
+    new (container: HTMLElement, events?: IEventEmitter): IView;
+}
